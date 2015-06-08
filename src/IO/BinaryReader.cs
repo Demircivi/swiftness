6603f91197c905f1e5041799d07d37b240923c49
florian0 <florian0@project-hazard.org>
2015-06-08 07:40:52 +0000

    Added BinaryReader-abstraction
    The .NET BinaryReader was not compatible with reading strings the
    way Joymax saves them.

---
+++ src/IO/BinaryReader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace Swiftness.IO
+{
+	/// <summary>
+	/// A BinaryReader abstraction that reads strings correctly
+	/// </summary>
+	class BinaryReader : System.IO.BinaryReader
+	{
+		public BinaryReader(System.IO.Stream input) 
+			: base(input) { }
+
+		public BinaryReader(System.IO.Stream input, Encoding encoding)
+			: base(input, encoding) { }
+
+
+		public override string ReadString()
+		{
+			int len = this.ReadInt32();
+				return new string(this.ReadChars(len));
+		}
+	}
+}

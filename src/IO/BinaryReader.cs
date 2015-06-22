fa2b525e7bc46e0f0fd046f464fdb09d8225d259
florian0 <florian0@project-hazard.org>
2015-06-22 13:58:04 +0000

    Added BinaryWriter and improved BinaryReader
    Added a writer that can write strings. BinaryReader now throws
    exceptions in case of malformatted data.

--- src/IO/BinaryReader.cs
+++ src/IO/BinaryReader.cs
@@ -14,11 +14,33 @@ namespace Swiftness.IO
 		public BinaryReader(System.IO.Stream input, Encoding encoding)
 			: base(input, encoding) { }
 
-
-		public override string ReadString()
+		/// <remarks>
+		/// Format: [4 Byte Length][X Byte String]
+		/// Its possible to read empty strings (length == 0)
+		/// </remarks>
+		/// <summary>
+		/// Reads a string from the stream using Joymax-binary-formatting.
+		/// </summary>
+		/// <returns>
+		/// The string.
+		/// </returns>
+		public override string ReadString ()
 		{
-			int len = this.ReadInt32();
-				return new string(this.ReadChars(len));
+			int len = this.ReadInt32 ();
+			
+			// Return an empty string, if there is no string to read
+			if (len == 0) {
+				return string.Empty;
+			}
+			
+			// Ignore negative lengths
+			// (Remark: The length could be unsigned integer, that would make sense. But i could
+			//          not find any evidence.)
+			if (len <= 0) {
+				throw new System.IO.IOException("Format Error: String length must be greater than 0");
+			}
+			
+			return new string(this.ReadChars(len));
 		}
 	}
 }
---
+++ src/IO/BinaryWriter.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Text;
+
+namespace Swiftness.IO
+{
+	public class BinaryWriter : System.IO.BinaryWriter
+	{
+		public BinaryWriter(System.IO.Stream input) 
+			: base(input) { }
+
+		public BinaryWriter(System.IO.Stream input, Encoding encoding)
+			: base(input, encoding) { }
+		
+		/// <remarks>
+		/// Format: [4 Byte Length][X Byte String]
+		/// Its possible to write empty strings (length == 0)
+		/// </remarks>
+		/// <summary>
+		/// Writes a string to the stream using Joymax-binary-formatting.
+		/// </summary>
+		/// <param name='value'>
+		/// The string to be written
+		/// </param>
+		public override void Write (string value)
+		{
+			int len = value.Length;
+			this.Write (len);
+			
+			for (int i = 0; i < value.Length; i++) {
+				this.Write (value [i]);
+			}
+		}
+		
+	}
+}
+

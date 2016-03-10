efa4b436a3906e49489b7da2e1dd044a2e69561d
florian0 <florian0@project-hazard.org>
2016-03-11 00:40:26 +0100

    Refactoring
    Refactored to match coding guidelines

--- examples/Serverlist/Program.cs
+++ examples/Serverlist/Program.cs
@@ -5,21 +5,19 @@ using Swiftness.Net;
 
 namespace Serverlist
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            TcpClient client = new TcpClient ();
-
-
-            client.Connect("5.39.47.168", 15779);
-			//client.Connect ("5.189.130.227", 15779);
+	class Program
+	{
+		static void Main (string[] args)
+		{
+			TcpClient client = new TcpClient ();
 			
+			client.Connect ("5.39.47.168", 15779);
+			//client.Connect ("5.189.130.227", 15779);
+
 			SilkroadClientStream stream = new SilkroadClientStream (client.GetStream ());
 			stream.Authenticate ();
 
-            Console.Read();
-			
-        }
-    }
+			Console.Read ();
+		}
+	}
 }

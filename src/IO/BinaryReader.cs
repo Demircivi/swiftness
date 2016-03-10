8071b404cbaca0936623591cb29776f2ac857f71
florian0 <florian0@project-hazard.org>
2016-03-11 00:41:36 +0100

    Refactoring
    Refactored code to match formatting guidelines

--- src/IO/BinaryReader.cs
+++ src/IO/BinaryReader.cs
@@ -10,84 +10,89 @@ namespace Swiftness.IO
 	/// </summary>
 	class BinaryReader : System.IO.BinaryReader
 	{
-		public BinaryReader(System.IO.Stream input)
-			: base(input) { }
+		public BinaryReader (System.IO.Stream input)
+			: base (input)
+		{ }
 
-		public BinaryReader(System.IO.Stream input, Encoding encoding)
-			: base(input, encoding) { }
+		public BinaryReader (System.IO.Stream input, Encoding encoding)
+			: base (input, encoding)
+		{ }
 
 
-        /// <remarks>
-        /// Format: [4 Byte Length][X Byte String]
-        /// Its possible to read empty strings (length == 0)
-        /// </remarks>
-        /// <summary>
-        /// Reads a string from the stream using Joymax-binary-formatting.
-        /// </summary>
-        /// <returns>
-        /// The string.
-        /// </returns>
-        public override string ReadString()
-        {
-            return this.ReadString(false);
-        }
+		/// <remarks>
+		/// Format: [4 Byte Length][X Byte String]
+		/// Its possible to read empty strings (length == 0)
+		/// </remarks>
+		/// <summary>
+		/// Reads a string from the stream using Joymax-binary-formatting.
+		/// </summary>
+		/// <returns>
+		/// The string.
+		/// </returns>
+		public override string ReadString ()
+		{
+			return this.ReadString (false);
+		}
 
-        /// <remarks>
-        /// Format: [2/4 Byte Length][X Byte String]
-        /// Its possible to read empty strings (length == 0)
-        /// </remarks>
-        /// <summary>
-        /// Reads a string from the stream using Joymax-binary-formatting.
-        /// </summary>
-        /// <param name="shortLength">Toggle between 2 and 4 byte length</param>
-        /// <returns>The string.</returns>
+		/// <remarks>
+		/// Format: [2/4 Byte Length][X Byte String]
+		/// Its possible to read empty strings (length == 0)
+		/// </remarks>
+		/// <summary>
+		/// Reads a string from the stream using Joymax-binary-formatting.
+		/// </summary>
+		/// <param name="shortLength">Toggle between 2 and 4 byte length</param>
+		/// <returns>The string.</returns>
 		public string ReadString (bool shortLength)
 		{
-            int len;
-            // Toggle between 2 and 4 byte length
-            if (shortLength)
-            {
-                len = this.ReadInt16();
-            }
-            else
-            {
-                len = this.ReadInt32();
-            }
-			
+			int len;
+			// Toggle between 2 and 4 byte length
+			if (shortLength)
+			{
+				len = this.ReadInt16 ();
+			}
+			else
+			{
+				len = this.ReadInt32 ();
+			}
+
 			// Return an empty string, if there is no string to read
-			if (len == 0) {
+			if (len == 0)
+			{
 				return string.Empty;
 			}
-			
+
 			// Ignore negative lengths
 			// (Remark: The length could be unsigned integer, that would make sense. But i could
 			//          not find any evidence.)
-			if (len <= 0) {
-				throw new System.IO.IOException("Format Error: String length must be greater than 0");
+			if (len <= 0)
+			{
+				throw new System.IO.IOException ("Format Error: String length must be greater than 0");
 			}
-			
-			return new string(this.ReadChars(len));
+
+			return new string (this.ReadChars (len));
 		}
-		
+
 		public string ReadString (int length)
 		{
 			return new string (this.ReadChars (length));
 		}
-		
+
 		/// <summary>
 		/// Reads a vector3 from stream
 		/// </summary>
 		/// <returns>
 		/// The vector3.
 		/// </returns>
-		public Vector3 ReadVector3 () {
-			float x = this.ReadSingle();
-			float y = this.ReadSingle();
-			float z = this.ReadSingle();
-			
+		public Vector3 ReadVector3 ()
+		{
+			float x = this.ReadSingle ();
+			float y = this.ReadSingle ();
+			float z = this.ReadSingle ();
+
 			return new Vector3 (x, y, z);
 		}
-		
+
 		/// <summary>
 		/// Reads a vector4 from stream
 		/// </summary>
@@ -99,8 +104,8 @@ namespace Swiftness.IO
 			float x = this.ReadSingle ();
 			float y = this.ReadSingle ();
 			float z = this.ReadSingle ();
-			float w = this.ReadSingle();
-			
+			float w = this.ReadSingle ();
+
 			return new Vector4 (w, x, y, z);
 		}
 	}
--- src/IO/BinaryWriter.cs
+++ src/IO/BinaryWriter.cs
@@ -5,12 +5,14 @@ namespace Swiftness.IO
 {
 	public class BinaryWriter : System.IO.BinaryWriter
 	{
-		public BinaryWriter(System.IO.Stream input) 
-			: base(input) { }
+		public BinaryWriter (System.IO.Stream input)
+			: base (input)
+		{ }
 
-		public BinaryWriter(System.IO.Stream input, Encoding encoding)
-			: base(input, encoding) { }
-		
+		public BinaryWriter (System.IO.Stream input, Encoding encoding)
+			: base (input, encoding)
+		{ }
+
 		/// <remarks>
 		/// Format: [4 Byte Length][X Byte String]
 		/// Its possible to write empty strings (length == 0)
@@ -25,12 +27,13 @@ namespace Swiftness.IO
 		{
 			int len = value.Length;
 			this.Write (len);
-			
-			for (int i = 0; i < value.Length; i++) {
-				this.Write (value [i]);
+
+			for (int i = 0; i < value.Length; i++)
+			{
+				this.Write (value[i]);
 			}
 		}
-		
+
 	}
 }
 
